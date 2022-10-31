4fa90eac6981f8e3a6e22fe1663bcb794d4c9c6d
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-31 20:54:57 +0600

    added form, fetch for post, newsuer inside post fetch and showing dynamic added user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/users`)
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  const handleSubmit = event => {
+    event.preventDefault()
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email }
+    console.log(user)
+
+    fetch(`http://localhost:5000/users` , {
+      method : "POST",
+      headers : {
+        'content-type' : 'application/json',
+      },
+      body : JSON.stringify(user)
+    })
+    .then(res => res.json())
+    .then(data => {
+      const newUser = [...users, data];
+      setUsers(newUser)
+    })
+    .catch(error => console.error(error))
+
+    event.target.reset()
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" required />
+        <br></br>
+        <input type="email" name="email" required />
+        <br></br>
+        <button>Add User</button>
+      </form>
+      <h2>
+        Users : {users.length}
+      </h2>
+      <div>
+        {
+          users.map(user => <p key={user.id}> {user.name}</p>)
+        }
+      </div>
     </div>
   );
 }
